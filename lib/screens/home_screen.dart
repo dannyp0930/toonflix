b214efa821ac13c5801f60dc7c350093720e864e
dannyp0930 <dannyp0930@gmail.com>
2023-02-13 14:07:47 +0900

    FEAT: Pause Play

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,19 +11,30 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int totalSeconds = 1500;
+  bool isRunning = false;
   late Timer timer;
 
   void onTick(Timer timer) {
     setState(() {
-      totalSeconds--;
+      totalSeconds = totalSeconds - 1;
     });
   }
 
-  void onStartaPress() {
+  void onStartaPressed() {
     timer = Timer.periodic(
       const Duration(seconds: 1),
       onTick,
     );
+    setState(() {
+      isRunning = true;
+    });
+  }
+
+  void onPausePressed() {
+    timer.cancel();
+    setState(() {
+      isRunning = false;
+    });
   }
 
   @override
@@ -50,8 +61,12 @@ class _HomeScreenState extends State<HomeScreen> {
             flex: 3,
             child: Center(
               child: IconButton(
-                onPressed: onStartaPress,
-                icon: const Icon(Icons.play_circle_outline),
+                onPressed: isRunning ? onPausePressed : onStartaPressed,
+                icon: Icon(
+                  isRunning
+                      ? Icons.pause_circle_outline
+                      : Icons.play_circle_outline,
+                ),
                 iconSize: 120,
                 color: Theme.of(context).cardColor,
               ),
